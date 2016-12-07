fb0fd2852ffb6691b3e879f1534f84fc035c5a90
sontt4 <sontt4@vng.com.vn>
2016-12-07 11:51:20 +0700

    Update

--- app/src/main/java/com/example/tts/myapplication/CustomList.java
+++ app/src/main/java/com/example/tts/myapplication/CustomList.java
@@ -3,11 +3,11 @@ package com.example.tts.myapplication;
 import android.app.Activity;
 import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 /**
  * Created by tts on 11/30/16.
@@ -26,24 +26,15 @@ public class CustomList extends ArrayAdapter<String> {
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = context.getLayoutInflater();
-
-        FrameLayout row = new FrameLayout(MyApplication.getAppContext());
-//        row.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-//        row.layout(0, 0, 540, 960);
-        ClippingImageView animatingImgView = new ClippingImageView(MyApplication.getAppContext());
-        animatingImgView.setImageBitmap(BitmapFactory.decodeResource(context.getResources(), imageId[position]));
-        animatingImgView.setRadius(5);
-        animatingImgView.setLeft(0);
-        animatingImgView.setTop(0);
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(300, 170);
+        FrameLayout cell = new FrameLayout(MyApplication.getAppContext());
+        ImageView imageView = new ImageView(MyApplication.getAppContext());
+        imageView.setTranslationX(AndroidUtilities.dp(100));
+        imageView.setImageBitmap(BitmapFactory.decodeResource(context.getResources(), imageId[position]));
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(AndroidUtilities.dp(250), AndroidUtilities.dp(140));
         params.topMargin = 5; params.bottomMargin = 5;
-//        row.setBackgroundColor(Color.argb(0, 255, 255, 255));
-//        row.getBackground().setAlpha(128);
-//        animatingImgView.setAlpha(1);
-        row.addView(animatingImgView, params);
+        cell.addView(imageView, params);
 
-        return row;
+        return cell;
     }
 
     @Override
--- app/src/main/java/com/example/tts/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tts/myapplication/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.tts.myapplication;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
@@ -12,7 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import static android.R.attr.orientation;
@@ -26,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.rsz_20160820_121959,
     };
 
+    FrameLayout animatingLayout;
+    ImageView staticImageView;
     ClippingImageView animatingImageView;
     FrameLayout main;
     ListView list;
@@ -34,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
     private ColorDrawable backgroundDrawable = new ColorDrawable(0xff000000);
 
     boolean isStatusBar = (Build.VERSION.SDK_INT >= 21);
+
+    int statusBarHeight;
+
     private boolean zoomIn = false;
 
 
@@ -42,51 +50,84 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//         CustomList adapter = new CustomList(MainActivity.this, imageId);
-//         list = (ListView) findViewById(R.id.list);
-//         list.setAdapter(adapter);
-//         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//             @Override
-//             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                 FrameLayout row = (FrameLayout) view;
-//                 ClippingImageView animatingImageView = (ClippingImageView) row.getChildAt(0);
-//                 Toast.makeText(MainActivity.this, "" + animatingImageView.getLeft() + " " + animatingImageView.getTop(), Toast.LENGTH_LONG).show();
-//                 openPhoto(drawRegion, animatingImageView, row, null);
-//             }
-//         });
+        statusBarHeight = getStatusBarHeight();
 
-
-        AndroidUtilities.checkDisplaySize(this, getResources().getConfiguration());
-        main = (FrameLayout) findViewById(R.id.activity_main);
-        backgroundDrawable.setAlpha(0);
-        main.setBackgroundDrawable(backgroundDrawable);
+        animatingLayout = (FrameLayout) findViewById(R.id.animation_layout);
         animatingImageView = new ClippingImageView(this);
-        Bitmap thumb = BitmapFactory.decodeResource(getResources(), R.drawable.rsz_20160731_101852);
-        animatingImageView.setImageBitmap(thumb);
+        backgroundDrawable.setAlpha(0);
+        animatingLayout.setBackgroundDrawable(backgroundDrawable);
+        animatingLayout.addView(animatingImageView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
-        main.addView(animatingImageView, new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
-        animatingImageView.setTranslationX(AndroidUtilities.dp(100));
-        animatingImageView.setTranslationY(AndroidUtilities.dp(-100));
-        ViewGroup.LayoutParams params = animatingImageView.getLayoutParams();
-        params.width = AndroidUtilities.dp(320);
-        params.height = AndroidUtilities.dp(180);
-        animatingImageView.setLayoutParams(params);
-        //imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-        animatingImageView.invalidate();
+        final int[] imgViewLocation = new int[2];
 
-        final Rect drawRegion = new Rect((int) animatingImageView.getTranslationX(), (int) animatingImageView.getTranslationY(),
-                (int) animatingImageView.getTranslationX() + animatingImageView.getWidth(), (int) animatingImageView.getTranslationY() + animatingImageView.getHeight());
-
-        animatingImageView.setOnClickListener(new View.OnClickListener() {
+        CustomList adapter = new CustomList(MainActivity.this, imageId);
+        list = (ListView) findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                openPhoto(new Rect((int) animatingImageView.getTranslationX(), (int) animatingImageView.getTranslationY(),
-                        (int) animatingImageView.getTranslationX() + animatingImageView.getWidth(), (int) animatingImageView.getTranslationY() + animatingImageView.getHeight()), animatingImageView, main, null);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                staticImageView = (ImageView) ((FrameLayout) view).getChildAt(0);
+                staticImageView.getLocationInWindow(imgViewLocation);
+                int right = imgViewLocation[0] + staticImageView.getWidth();
+                int bottom = imgViewLocation[1] + staticImageView.getHeight();
+                Log.d("Loc ", imgViewLocation[0] + " " + imgViewLocation[1] + " " + right + " " + bottom);
+
+                staticImageView.buildDrawingCache();
+                Bitmap bm = Bitmap.createBitmap(staticImageView.getDrawingCache());
+                staticImageView.destroyDrawingCache();
+
+                animatingImageView.setTranslationX(imgViewLocation[0]);
+                animatingImageView.setTranslationY(imgViewLocation[1]);
+                ViewGroup.LayoutParams params = animatingImageView.getLayoutParams();
+                params.width = staticImageView.getWidth();
+                params.height = staticImageView.getHeight();
+                animatingImageView.setLayoutParams(params);
+
+                Rect drawRegion = new Rect(imgViewLocation[0], imgViewLocation[1], right, bottom);
+                openPhoto(drawRegion, animatingImageView, animatingLayout, null, new PlaceHolder(bm));
             }
         });
 
+
+//        AndroidUtilities.checkDisplaySize(this, getResources().getConfiguration());
+//        main = (FrameLayout) findViewById(R.id.main_frame_layout);
+//        backgroundDrawable.setAlpha(0);
+//        main.setBackgroundDrawable(backgroundDrawable);
+//        animatingImageView = new ClippingImageView(this);
+//        Bitmap thumb = BitmapFactory.decodeResource(getResources(), R.drawable.rsz_20160731_101852);
+//        animatingImageView.setImageBitmap(thumb);
+//
+//        main.addView(animatingImageView, new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+//        animatingImageView.setTranslationX(AndroidUtilities.dp(100));
+//        animatingImageView.setTranslationY(AndroidUtilities.dp(-100));
+//        ViewGroup.LayoutParams params = animatingImageView.getLayoutParams();
+//        params.width = AndroidUtilities.dp(320);
+//        params.height = AndroidUtilities.dp(180);
+//        animatingImageView.setLayoutParams(params);
+//        //imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+//        animatingImageView.invalidate();
+//
+//        final Rect drawRegion = new Rect((int) animatingImageView.getTranslationX(), (int) animatingImageView.getTranslationY(),
+//                (int) animatingImageView.getTranslationX() + animatingImageView.getWidth(), (int) animatingImageView.getTranslationY() + animatingImageView.getHeight());
+//
+//        animatingImageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                openPhoto(new Rect((int) animatingImageView.getTranslationX(), (int) animatingImageView.getTranslationY(),
+//                        (int) animatingImageView.getTranslationX() + animatingImageView.getWidth(), (int) animatingImageView.getTranslationY() + animatingImageView.getHeight()), animatingImageView, main, null);
+//            }
+//        });
+
         Log.d("MyLog", "Screen Pixels: " + AndroidUtilities.displaySizePixel.x + " " + AndroidUtilities.displaySizePixel.y);
-        Log.d("MyLog", "ImageView: " + animatingImageView.getLeft() + " " + animatingImageView.getTop() + " " + animatingImageView.getWidth() + " " + animatingImageView.getHeight());
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 
     public class PlaceHolder {
@@ -101,17 +142,16 @@ public class MainActivity extends AppCompatActivity {
         public int clipTopAddition;
         public float scale = 1.0f;
 
-        PlaceHolder() {
+        PlaceHolder(Bitmap bitmap) {
             viewX = 0; viewY = 372;
-            radius = AndroidUtilities.dp(50);
-            thumb = BitmapFactory.decodeResource(getResources(), R.drawable.rsz_20160731_101852);
+            radius = AndroidUtilities.dp(2);
+            thumb = bitmap;
         }
     }
 
-    private void openPhoto(Rect drawRegion, ClippingImageView animatingImageView, FrameLayout parent, Bitmap fullImage) {
+    private void openPhoto(Rect drawRegion, final ClippingImageView animatingImageView, FrameLayout parent, Bitmap fullImage, PlaceHolder object) {
         //Rect drawRegion = new Rect(139,11,517,223);
         zoomIn = true;
-        PlaceHolder object = new PlaceHolder();
 
         animatingImageView.setAnimationValues(animationValues);
 
@@ -134,40 +174,42 @@ public class MainActivity extends AppCompatActivity {
         animatingImageView.setLayoutParams(layoutParams);
 
         float scaleX = (float) AndroidUtilities.displaySize.x / layoutParams.width;
-        float scaleY = (float) (AndroidUtilities.displaySize.y + (isStatusBar ? AndroidUtilities.statusBarHeight : 0)) / layoutParams.height;
+        float scaleY = (float) (AndroidUtilities.displaySize.y + (isStatusBar ? statusBarHeight : 0)) / layoutParams.height;
         float scale = scaleX > scaleY ? scaleY : scaleX;
         float width = layoutParams.width * scale;        //width  of full image
         float height = layoutParams.height * scale;      //height of full image
         float xPos = (AndroidUtilities.displaySize.x - width) / 2.0f;
-        float yPos = ((AndroidUtilities.displaySize.y + (isStatusBar ? AndroidUtilities.statusBarHeight : 0)) - height) / 2.0f;
+        float yPos = ((AndroidUtilities.displaySize.y + (isStatusBar ? statusBarHeight : 0)) - height) / 2.0f;
 //        int clipHorizontal = Math.abs(drawRegion.left - object.imageReceiver.getImageX());
 //        int clipVertical = Math.abs(drawRegion.top - object.imageReceiver.getImageY());
 
         int coords2[] = new int[2];
         parent.getLocationInWindow(coords2);
-        int clipTop = coords2[1] - (isStatusBar ? 0 : AndroidUtilities.statusBarHeight) - (object.viewY + drawRegion.top) + object.clipTopAddition;
-        if (clipTop < 0) {
-            clipTop = 0;
-        }
-        int clipBottom = (object.viewY + drawRegion.top + layoutParams.height) - (coords2[1] + parent.getHeight() - (isStatusBar ? 0 : AndroidUtilities.statusBarHeight)) + object.clipBottomAddition;
-        if (clipBottom < 0) {
-            clipBottom = 0;
-        }
-        clipBottom = 0;
+        int clipTop = 0;
+        int clipBottom = 0;
+//        int clipTop = coords2[1] - (isStatusBar ? 0 : statusBarHeight) - (object.viewY + drawRegion.top) + object.clipTopAddition;
+//        if (clipTop < 0) {
+//            clipTop = 0;
+//        }
+//        int clipBottom = (object.viewY + drawRegion.top + layoutParams.height) - (coords2[1] + parent.getHeight() - (isStatusBar ? 0 : statusBarHeight)) + object.clipBottomAddition;
+//        if (clipBottom < 0) {
+//            clipBottom = 0;
+//        }
+//        clipBottom = 0;
 //        clipTop = Math.max(clipTop, clipVertical);
 //        clipBottom = Math.max(clipBottom, clipVertical);
 
         if (drawRegion.bottom > AndroidUtilities.displaySize.y) {
-            clipBottom = drawRegion.bottom - AndroidUtilities.displaySize.y;
+            clipBottom = drawRegion.bottom - (AndroidUtilities.displaySize.y + (isStatusBar ? statusBarHeight : 0));
         }
         if (drawRegion.top < 0) {
-            clipTop = -drawRegion.top;
+            clipTop = -drawRegion.top + (isStatusBar ? statusBarHeight : 0);
         }
 
         animationValues[0][0] = animatingImageView.getScaleX();
         animationValues[0][1] = animatingImageView.getScaleY();
         animationValues[0][2] = animatingImageView.getTranslationX();
-        animationValues[0][3] = animatingImageView.getTranslationY();
+        animationValues[0][3] = animatingImageView.getTranslationY() - (isStatusBar ? statusBarHeight : 0);
 //        animationValues[0][2] = coords2[0] + animatingImageView.getLeft();
 //        animationValues[0][3] = coords2[1] + animatingImageView.getTop();
 //        animationValues[0][2] = animatingImageView.getLeft();
@@ -207,10 +249,16 @@ public class MainActivity extends AppCompatActivity {
                 ObjectAnimator.ofInt(backgroundDrawable, "alpha", 0, 255)
 //                ObjectAnimator.ofFloat(parent, "alpha", 0.0f, 1.0f)
         );
-        animatorSet.setDuration(2000);
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                staticImageView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        animatorSet.setDuration(200);
         animatorSet.start();
 
-        //animatingImageView.setImageBitmap(fullImage);
     }
 
     @Override
@@ -234,7 +282,16 @@ public class MainActivity extends AppCompatActivity {
                     ObjectAnimator.ofInt(backgroundDrawable, "alpha", 255, 0)
 //                ObjectAnimator.ofFloat(parent, "alpha", 0.0f, 1.0f)
             );
-            animatorSet.setDuration(1500);
+
+            animatorSet.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    animatingImageView.setVisibility(View.GONE);
+                    staticImageView.setVisibility(View.VISIBLE);
+                }
+            });
+
+            animatorSet.setDuration(200);
             animatorSet.start();
         }
     }
