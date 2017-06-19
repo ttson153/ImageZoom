8d50c5d8722cb0d3a5b5acb9b71acf6912ed9a64
sontt4 <sontt4@vng.com.vn>
2017-06-19 15:57:33 +0700

    Clean code

--- libimagezoomanim/src/main/java/com/tts/libimagezoomanim/AnimationController.java
+++ libimagezoomanim/src/main/java/com/tts/libimagezoomanim/AnimationController.java
@@ -17,8 +17,6 @@ public class AnimationController implements AnimationLayout.OnEventListener{
     private int[] imgViewLocation = new int[2];
     private Rect drawRegion;
     private TransformData currentPlaceHolder;
-//    private int clipTopAddition = AndroidUtilities.dp(56);
-//    private int clipBottomAddition = AndroidUtilities.dp(0);
 
     private Context mContext;
     private AnimationLayout mAnimationLayout;
@@ -48,37 +46,8 @@ public class AnimationController implements AnimationLayout.OnEventListener{
         int bottom = top + currentAnimationView.getHeight();
         Log.d("Loc ", imgViewLocation[0] + " " + imgViewLocation[1] + " " + right + " " + bottom);
 
-//        Bitmap bm = BitmapFactory.decodeResource(mContext.getResources(), mImageId[position]);
-
-//        currentAnimationView.setDrawingCacheEnabled(true);
-//        currentAnimationView.buildDrawingCache();
-//        currentAnimationView.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
-//                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-//        currentAnimationView.layout(0, 0, currentAnimationView.getMeasuredWidth(), currentAnimationView.getMeasuredHeight());
-//        Bitmap bm = Bitmap.createBitmap(currentAnimationView.getDrawingCache());
-//        currentAnimationView.destroyDrawingCache();
-//        currentAnimationView.setDrawingCacheEnabled(false);
-//
-//        ViewGroup.LayoutParams params = currentAnimationView.getLayoutParams();
-
         drawRegion = new Rect(left, top, right, bottom);
         currentPlaceHolder = data;
-//        if (currentPlaceHolder != null) {
-//            currentPlaceHolder.getThumbImage().recycle();
-//        }
-//        if (position == 4) {
-//            currentPlaceHolder = new TransformData()
-//                    .setThumbImage(bm)
-//                    .setRadius(params.width / 2)
-//                    .setClipTopAddition(clipTopAddition)
-//                    .setClipBottomAddition(clipBottomAddition);
-//        } else {
-//            currentPlaceHolder = new TransformData()
-//                    .setThumbImage(bm)
-//                    .setClipTopAddition(clipTopAddition)
-//                    .setClipBottomAddition(clipBottomAddition);
-//        }
-
         mAnimationLayout.expand(drawRegion, currentPlaceHolder);
     }
 
@@ -90,40 +59,6 @@ public class AnimationController implements AnimationLayout.OnEventListener{
             int right = left + currentAnimationView.getWidth();
             int bottom = top + currentAnimationView.getHeight();
             drawRegion = new Rect(left, top, right, bottom);
-
-//            Bitmap bm = BitmapFactory.decodeResource(mContext.getResources(), mImageId[currentPosition]);
-//            currentAnimationView.setDrawingCacheEnabled(true);
-//            currentAnimationView.buildDrawingCache();
-//            currentAnimationView.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
-//                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-//            currentAnimationView.layout(0, 0, currentAnimationView.getMeasuredWidth(), currentAnimationView.getMeasuredHeight());
-//            Bitmap bm = Bitmap.createBitmap(currentAnimationView.getDrawingCache());
-//            currentAnimationView.destroyDrawingCache();
-//            currentAnimationView.setDrawingCacheEnabled(false);
-//
-//            ViewGroup.LayoutParams params = currentAnimationView.getLayoutParams();
-//
-//            if (currentPlaceHolder != null) {
-//                currentPlaceHolder.getThumbImage().recycle();
-//            }
-//
-//            if (currentPosition == 4) {
-//                currentPlaceHolder = new TransformData()
-//                        .setThumbImage(bm)
-//                        .setRadius(params.width / 2)
-//                        .setClipTopAddition(clipTopAddition)
-//                        .setClipBottomAddition(clipBottomAddition);
-//            } else {
-//                currentPlaceHolder = new TransformData()
-//                        .setThumbImage(bm)
-//                        .setClipTopAddition(clipTopAddition)
-//                        .setClipBottomAddition(clipBottomAddition);
-//            }
-//                currentImageView.setVisibility(View.INVISIBLE);
-//        } else {
-//            drawRegion = null;
-//            currentPlaceHolder = null;
-//        }
 
             mAnimationLayout.shrink(drawRegion, currentPlaceHolder);
         }
